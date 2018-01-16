1b70a0bcf723f14af7633c71d78c01a086dde2ba
rnowak4 <radek095@gmail.com>
2018-01-16 23:47:12 +0100

    Removed syf

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-//@EnableScheduling
-// TODO zapis do pliku po unlock
-// TODO zwolnienie pamieci jak plik nie czytany przez nikogo
+@EnableScheduling
 public class Server {
 
     public static void main(final String[] args){
